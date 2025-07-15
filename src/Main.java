80886b1ecb3160fd0b7cf7980693a66ea171c3f1
thommy <ngothommy@gmail.com>
2025-07-16 01:36:29 +0200

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import java.util.Arrays;
+import Training.DataReader;
 
 public class Main {
     public static void main(String[] args) {
-        DataReader data = new DataReader();
+
     }
 }
---
+++ src/Training/Layer.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class Layer {
+    float[][] weights;
+    float[] bias;
+    float[] input;
+
+    public Layer(int n, float[] input){
+        this.input = input;
+        this.weights = new float[n][input.length];
+        this.bias = new float[n];
+    }
+
+}
