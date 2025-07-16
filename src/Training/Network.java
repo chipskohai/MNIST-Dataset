d8e1b8f69ea40305771df598a7f88f064706d4fb
thommy <ngothommy@gmail.com>
2025-07-16 21:32:01 +0200

    Working on layers and neurons

--- src/Training/Network.java
+++ src/Training/Network.java
@@ -1,5 +1,13 @@
 package Training;
 
 public class Network {
+    private float[] inputLayer;
+    private Layer hiddenLayer;
+    private Layer outputLayer;
+    private DataReader data;
 
+    public Network(){
+        data = new DataReader();
+
+    }
 }
